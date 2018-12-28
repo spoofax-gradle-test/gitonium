e260cd1db2528c41a1606c6efc31373873fa3403
Gabriel Konat <gabrielkonat@gmail.com>
2018-12-28 15:30:28 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  // Stick with version 1.3.10 because the kotlin-dsl plugin uses that.
+  kotlin("jvm") version "1.3.10" apply true
+  `kotlin-dsl`
+  `java-gradle-plugin`
+}
+
+group = "org.metaborg"
+version = "develop-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+dependencies {
+  compile(kotlin("stdlib"))
+}
+
+kotlinDslPluginOptions {
+  experimentalWarning.set(false)
+}
+tasks.withType<KotlinCompile>().all {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
+gradlePlugin {
+  plugins {
+    create("gitonium") {
+      id = "org.metaborg.gitonium"
+      implementationClass = "mb.gitonium.GitoniumPlugin"
+    }
+  }
+}
+
+tasks {
+  register("buildAll") {
+    dependsOn("build")
+  }
+  register("cleanAll") {
+    dependsOn("clean")
+  }
+}
