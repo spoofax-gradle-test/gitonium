d9b5b6992a954e0f969856486c0e9da4209eb6e4
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-02 12:31:16 +0100

    Temporarily force set version to 0.2.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "org.metaborg"
-version = the<GitoniumExtension>().version
+version = "0.2.0" //the<GitoniumExtension>().version
 
 repositories {
   maven(url = "http://home.gohla.nl:8091/artifactory/all/")
