2040281337a92e5b48b7fa1c84465a41f989047f
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-11 14:45:48 +0100

    Cleanup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,11 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-  id("org.metaborg.gradle.config") version "0.4.1"
+  id("org.metaborg.gradle.config") version "0.4.3"
   id("org.metaborg.gitonium") version "0.3.0" // Bootstrap with previous version.
   kotlin("jvm") version "1.3.11" // Use version 1.3.11 for compatibility with Gradle 5.1.
   `kotlin-dsl`
   `java-gradle-plugin`
   `maven-publish`
 }
-
-// TODO: configureKotlinGradlePlugin() causes compilation errors in plugin.
-//metaborgConfig {
-//  configureKotlinGradlePlugin()
-//}
 
 dependencies {
   compile("org.eclipse.jgit:org.eclipse.jgit:5.2.0.201812061821-r")
